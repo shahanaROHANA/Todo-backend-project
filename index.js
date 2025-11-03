67c151b7f2aaa7d082ea05695df1d5d55ca09b26
shahanaROHANA <kokilarubanshakana@gmail.com>
2025-11-03 10:17:31 +0530

    my second

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 import express from 'express';
    import mongoose from 'mongoose';
+import cors from 'cors'
+
+
    const app= express();
+app.use(cors({
+
+}))
+
    app.use(express.json());
 const todoSchema=new mongoose.Schema({
     id:Number,
